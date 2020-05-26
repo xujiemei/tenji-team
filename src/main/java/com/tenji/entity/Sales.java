83c542bcda2fe5e3c7fbd74a1136d9dac8cac1f3
tenji <xx branch>
2020-05-26 15:21:32 +0900

    '(mmm)'

---
+++ src/main/java/com/tenji/entity/Sales.java
@@ -0,0 +1,60 @@
+package com.tenji.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by tenji
+ * Time: 2020/04/13 17:56
+ */
+
+public class Sales implements Serializable{
+
+    private String usercd;
+    private String username;
+    private String sex;
+    private Date birthday;
+    public Sales(){
+    }
+
+    public String getusercd() {
+        return usercd;
+    }
+
+    public void setusercd(String strusercd) {
+        this.usercd = strusercd;
+    }
+
+    public String getusername() {
+        return username;
+    }
+
+    public void setusername(String strusername) {
+        this.username = strusername;
+    }
+
+    public String getsex() {
+        return sex;
+    }
+
+    public void setsex(String strsex) {
+        this.sex = strsex;
+    }
+
+    public Date getbirthday() {
+        return birthday;
+    }
+
+    public void setbirthday(Date datebirthday) {
+        this.birthday = datebirthday;
+    }
+    /*
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "usercd=" + usercd +
+                ", username='" + username + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }*/
+}
---
+++ src/main/java/com/tenji/service/SalesListService.java
@@ -0,0 +1,99 @@
+package com.tenji.service;
+
+import com.tenji.AppConfig;
+import com.tenji.entity.Employee;
+import com.tenji.entity.Sales;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+
+public class SalesListService {
+
+    @Autowired
+    AppConfig appConfig;
+
+    public Sales findEmployeeByCd(String userCD) {
+        try (Connection connection = appConfig.dataSource().getConnection()) {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT UserCD,UserName,Sex FROM m_Employee where Usercd='" + userCD + "'");
+
+            Sales emp = new Sales();
+
+            while (rs.next()) {
+                emp.setusercd(rs.getString("usercd"));
+                emp.setusername(rs.getString("username"));
+                emp.setsex(rs.getString("Sex"));
+            }
+            return emp;
+
+        } catch (Exception e) {
+            Sales emp = new Sales();
+            emp.setusercd("nocd");
+            emp.setusername("noname");
+            return emp;
+        }
+    }
+
+    public List<Employee> findEmployeeList(Sales empPara) {
+        try (Connection connection = appConfig.dataSource().getConnection()) {
+
+            List<Employee> emplist = new ArrayList<Employee>();
+
+            Statement stmt = connection.createStatement();
+
+            String strSql;
+            if(getWhere(empPara) != "") {
+                strSql = "SELECT UserCD,UserName,Sex FROM m_Employee where " + getWhere(empPara);
+                System.out.println("strSql.:" + strSql);
+            }else{
+                strSql = "SELECT UserCD,UserName,Sex FROM m_Employee ";
+            }
+            ResultSet rs = stmt.executeQuery(strSql);
+
+            while (rs.next()) {
+                Employee emp = new Employee();
+                emp.setusercd(rs.getString("usercd"));
+                emp.setusername(rs.getString("username"));
+                emp.setsex(rs.getString("Sex"));
+            }
+
+            return emplist;
+
+        } catch (Exception e) {
+            List<Employee> emplist = new ArrayList<Employee>();
+            Employee emp = new Employee();
+            emp.setusercd("nocd");
+            emp.setusername("noname");
+            emplist.add(emp);
+            return emplist;
+        }
+    }
+
+    //レコード検索条件：WHERE句設定
+    private String getWhere(Sales empPara){
+        List<String> param = new ArrayList<String>();
+
+        //Usercd
+        if(empPara.getusercd() != ""){
+            param.add("Usercd LIKE '%" + empPara.getusercd() + "%'");
+        }
+        //UserName
+        if(empPara.getusername() != ""){
+            param.add("UserName LIKE '%" + empPara.getusername() + "%'");
+        }
+
+        if(param.size()>0){
+            return String.join( " AND ",param);
+        }else{
+            return "";
+        }
+    }
+}
