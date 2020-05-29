82fa472a8240a37418d001dd38fe238953ecdd03
tenji <xx branch>
2020-05-29 14:20:16 +0900

    '(1420)'

---
+++ src/main/java/com/tenji/controller/SalesListController.java
@@ -0,0 +1,25 @@
+package com.tenji.controller;
+
+import com.tenji.entity.Employee;
+import com.tenji.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+@Controller
+public class SalesListController {
+    @RequestMapping(path = "/salesList")
+    public String salesList(){
+        return"SalesList";
+    }
+
+
+}
+
